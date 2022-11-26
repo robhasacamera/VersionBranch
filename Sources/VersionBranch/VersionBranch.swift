db0efbee8ab4653e8680383779a1713e24eeac4a
Robert Cole <robhasacamera@gmail.com>
2022-11-26 12:24:57 -0800

    Adding verification

--- Sources/VersionBranch/VersionBranch.swift
+++ Sources/VersionBranch/VersionBranch.swift
@@ -27,12 +27,33 @@
 import ArgumentParser
 import Foundation
 
+enum Git {
+    static let isGitRepo = "git rev-parse --is-inside-work-tree --quiet"
+
+    // Will print hash if it exists, otherwise nothing
+    static let doesMainExist = "git rev-parse --verify --quiet main"
+    static let checkoutMain = "git checkout main"
+    static let main = "main"
+
+    static let lastestTag = "git describe --tags --abbrev=0"
+
+    static let currentBranch = "git rev-parse --abbrev-ref HEAD"
+
+    static let localBranchHash = "git rev-parse @"
+    static let remoteBranchHash = "git rev-parse @{u}"
+    // gives you the hash you need to merge on top of, if local hash is equal, pull, otherwise a push is needed and we'll just ignore this
+    // https://stackoverflow.com/questions/3258243/check-if-pull-needed-in-git
+    static let mergeBaseHash = "git merge-base @ @{u}"
+
+    static let pull = "git pull"
+}
+
 // https://betterprogramming.pub/creating-a-swifty-command-line-tool-with-argumentparser-a6240b512b0b
 // - Beginning tutorial
 // https://rderik.com/blog/understanding-the-swift-argument-parser-and-working-with-stdin/
 // - Getting standard input
 struct VersionBranch: ParsableCommand {
-    static let lastestTagCommand = "git describe --tags --abbrev=0"
+    static let scriptName = "versionbranch"
 
     static let configuration = CommandConfiguration(
         commandName: "versionbranch",
@@ -56,6 +77,10 @@ struct VersionBranch: ParsableCommand {
     @Flag(name: .shortAndLong, help: "Increments version number to the next fix version: 1.0.0 would become 1.0.1")
     var fix = false
 
+    // TODO: Will need to verify string is in correct format, verify path exists, then run the comment in that directly (probably some bash arument
+    @Argument(help: "The path to the git repo.")
+    var gitPath: String?
+
     // TODO: Add option to provide the filepath to repo
     // TODO: Provide an option to automatically checkout main and do a pull
 
@@ -64,11 +89,59 @@ struct VersionBranch: ParsableCommand {
     // TODO: Check if git repo
     // TODO: Check if latest tag matches regex
     func validate() throws {
-        print("first")
+        var errors = [String]()
+
+        if try shell(Git.isGitRepo) == "true" {
+            if try !shell(Git.doesMainExist).isEmpty {
+
+                // Move tag check to different function. It can provide the version components, 0.0.0 if no previous tags exist, or throws if the format is incorrect
+                let latestTag = try shell(Git.lastestTag)
+
+                if !latestTag.isEmpty {
+
+                    if #available(macOS 13.0, *) {
+                        if latestTag.matches(of: #/\d+\.\d+\.\d+/#).isEmpty {
+                            errors.append("Tags must be in the format X.X.X, examples: 1.0.1, 2.3.0, 0.0.1")
+                        }
+                    } else {
+                        errors.append("\(Self.scriptName) requires Mac 13.0+.")
+                    }
+                }
+            } else {
+                errors.append("The main branch does not exist. \(Self.scriptName) requires a main branch to exist")
+            }
+        } else {
+            errors.append("This is not a git repo. \(Self.scriptName) must be run within a git repo.")
+        }
+
+        let versionFlags = [major, minor, fix]
+
+        var trueCount = 0
+
+        for flag in versionFlags {
+            if flag {
+                trueCount += 1
+            }
+        }
+
+        if trueCount < 1 {
+            errors.append("A version flag must be provided, use -m (--major), -n (--minor), or -f (--fix).")
+        } else if trueCount > 1 {
+            errors.append("Too many version flags provided. Please provide only one version flag, -m (--major), -n (--minor), or -f (--fix).")
+        }
+
+        guard errors.isEmpty else {
+            for error in errors {
+                print(error)
+            }
+            throw ExitCode.validationFailure
+        }
+
+        throw ExitCode.success
     }
 
     mutating func run() throws {
-        print(try shell(Self.lastestTagCommand))
+
 
         // TODO: Check if on main, if not, prompt to checkout main
         // TODO: Check if needs pull, if so, prompt to pull
@@ -92,22 +165,10 @@ struct VersionBranch: ParsableCommand {
         let data = pipe.fileHandleForReading.readDataToEndOfFile()
 
         guard let output = String(data: data, encoding: .utf8) else {
-            throw CustomError.shellOutputFailed
+            print("Could not get output for command: \(command)")
+            throw ExitCode.failure
         }
 
         return output
     }
-}
-
-struct RuntimeError: Error, CustomStringConvertible {
-    var description: String
-
-    init(_ description: String) {
-        self.description = description
-    }
-}
-
-// TODO: Rename
-enum CustomError: Swift.Error {
-    case shellOutputFailed
 }
